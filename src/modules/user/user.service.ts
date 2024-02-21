6b5e5ec3e8b329b9ed1196cf0c800a2e467b68e6
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-02-21 12:19:27 +0200

    les1

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/request/create-user.dto';
+import { UpdateUserDto } from './dto/request/update-user.dto';
+
+@Injectable()
+export class UserService {
+  public async create(createUserDto: CreateUserDto): Promise<any> {
+    return 'This action adds a new user';
+  }
+
+  public async findAll(): Promise<string> {
+    return `This action returns all user`;
+  }
+
+  public async findOne(id: number): Promise<string> {
+    return `This action returns a #${id} user`;
+  }
+
+  public async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<string> {
+    return `This action updates a #${id} user`;
+  }
+
+  public async remove(id: number): Promise<string> {
+    return `This action removes a #${id} user`;
+  }
+}
